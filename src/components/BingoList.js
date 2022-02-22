4bb2a480688a040f48ba2897d25fbcf23de8e0de
Oskar Lindblad <lindbladoskar@gmail.com>
2022-02-22 22:15:06 +0100

    New startmenu

--- src/components/BingoList.js
+++ src/components/BingoList.js
@@ -1,39 +1,21 @@
 import React, { useState, useEffect } from "react";
 import SanityClient from "../client";
 import { Link } from "react-router-dom";
-import BlockContent from "@sanity/block-content-to-react";
-
 import Popover from "@mui/material/Popover";
-import Button from "@mui/material/Button";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
+import PopupState, { bindTrigger, bindPopover } from "material-ui-popup-state";
 
-import Accordion from "@mui/material/Accordion";
-import AccordionSummary from "@mui/material/AccordionSummary";
-import AccordionDetails from "@mui/material/AccordionDetails";
+import Card from "@mui/material/Card";
+
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
 
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 import PlayCircleFilled from "@mui/icons-material/PlayCircleFilled";
+import HelpOutlineIcon from "@mui/icons-material/HelpOutline";
 import Print from "@mui/icons-material/Print";
 
 export default function Post() {
   const [bingoData, setBingoData] = useState(null);
-
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
-
-  const open = Boolean(anchorEl);
-  const id = open ? "simple-popover" : undefined;
 
   useEffect(() => {
     SanityClient.fetch(
@@ -50,65 +32,87 @@ export default function Post() {
   return (
     <Container maxWidth="sm">
       <Box>
-        <Typography variant={"h4"} className="title-hompage" color="primary">
+        <Typography
+          sx={{ marginTop: "0.5em" }}
+          variant={"h4"}
+          className="title-hompage"
+          color="primary"
+        >
           Bingo
         </Typography>
         <div>
           {bingoData &&
             bingoData.map((bingo, index) => (
-              <Card key={index} sx={{ marginTop: "0.5em" }}>
-                <Typography
-                  variant="h6"
-                  component="div"
-                  sx={{ margin: "0.2em" }}
-                  color="primary"
-                >
-                  {bingo.title}
-                </Typography>
-
-                <Popover
-                  id={id}
-                  open={open}
-                  anchorEl={anchorEl}
-                  onClose={handleClose}
-                  anchorOrigin={{
-                    vertical: "bottom",
-                    horizontal: "left",
+              <Card
+                key={index}
+                sx={{
+                  marginTop: "0.5em",
+                  padding: "0 0.5em",
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Box>
+                  <Typography
+                    variant="h5"
+                    component="div"
+                    sx={{ margin: " 0.4em 0.2em 0.2em 0.2em" }}
+                    color="primary"
+                  >
+                    {bingo.title}
+                  </Typography>
+                </Box>
+                <Box
+                  sx={{
+                    display: "flex",
+                    marginTop: "0.5em",
                   }}
                 >
-                  <Typography sx={{ p: 2 }}>
-                    The content of the Popover.
-                  </Typography>
-                </Popover>
+                  <Print
+                    color="primary"
+                    fontSize="large"
+                    sx={{ marginLeft: "0.2em" }}
+                  />
 
-                <Link to={"/" + bingo.slug.current} key={bingo.slug.current}>
-                  <PlayCircleFilled color="primary" />
-                </Link>
-                {/*               <Accordion>
-                  <AccordionSummary
-                    expandIcon={<ExpandMoreIcon />}
-                    aria-controls="panel1a-content"
-                    id="panel1a-header"
-                  >
-                    <Typography>{bingo.title}&#160;&#160; </Typography>
-                    <Link
-                      to={"/" + bingo.slug.current}
-                      key={bingo.slug.current}
-                    >
-                      <PlayCircleFilled color="primary" />
-                    </Link>
-                  </AccordionSummary>
-                  <AccordionDetails>
-                    {bingo.description && (
-                      <BlockContent
-                        blocks={bingo.description}
-                        projectId={process.env.REACT_APP_PROJECTID}
-                        dataset="production"
-                      />
-                    )}
-                    <Print />
-                  </AccordionDetails>
-                    </Accordion>*/}
+                  {bingo.description && (
+                    <PopupState variant="popover" popupId="popup-popover">
+                      {(popupState) => (
+                        <div>
+                          <HelpOutlineIcon
+                            variant="contained"
+                            {...bindTrigger(popupState)}
+                            color="primary"
+                            fontSize="large"
+                            sx={{ marginLeft: "0.2em" }}
+                          />
+                          <Popover
+                            {...bindPopover(popupState)}
+                            anchorOrigin={{
+                              vertical: "bottom",
+                              horizontal: "center",
+                            }}
+                            transformOrigin={{
+                              vertical: "top",
+                              horizontal: "center",
+                            }}
+                          >
+                            <Typography sx={{ p: 2 }}>
+                              {bingo.description}{" "}
+                            </Typography>
+                          </Popover>
+                        </div>
+                      )}
+                    </PopupState>
+                  )}
+
+                  <Link to={"/" + bingo.slug.current} key={bingo.slug.current}>
+                    <PlayCircleFilled
+                      color="primary"
+                      fontSize="large"
+                      sx={{ marginLeft: "0.2em" }}
+                    />
+                  </Link>
+                </Box>
               </Card>
             ))}
         </div>
